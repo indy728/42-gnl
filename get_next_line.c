f90565f1c2c544c97fc5ea39384631b4a98e6b54
Kyle MURRAY <kmurray@e1z2r3p25.42.us.org>
2017-01-07 15:11:25 -0800

    initial norm attempt

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,20 @@
 /*   By: kmurray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/28 18:21:17 by kmurray           #+#    #+#             */
-/*   Updated: 2017/01/06 20:44:25 by kmurray          ###   ########.fr       */
+/*   Updated: 2017/01/07 13:46:20 by kmurray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			get_next_line(const int fd, char **line)
+static int	read_bytes(int fd, int j, char *dup, char *ptr)
 {
+	int		i;
 	int		bytes_read;
 	char	buf[BUFF_SIZE + 1];
-	int 	i = 0;
-	int		j = 0;
-	static char	*dup;
-	char *ptr = *line;
 
+	i = 0;
 	ft_bzero(buf, BUFF_SIZE + 1);
-	if (dup != NULL)
-	{
-		while (dup[j])
-		{
-			if (dup[j] == '\n')
-			{
-				dup = dup + j + 1;
-				return (1);
-			}
-			ft_bzero(ptr + j, ft_strlen(dup));
-			ptr[j] = dup[j];
-			j++;
-		}
-	}
 	while ((bytes_read = read(fd, buf, BUFF_SIZE)))
 	{
 		while (buf[i])
@@ -52,5 +36,32 @@ int			get_next_line(const int fd, char **line)
 		}
 		i = 0;
 	}
+	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	int			j;
+	static char	*dup;
+	char		*ptr;
+
+	j = 0;
+	ptr = *line;
+	if (dup != NULL)
+	{
+		while (dup[j])
+		{
+			if (dup[j] == '\n')
+			{
+				dup = dup + j + 1;
+				return (1);
+			}
+			ft_bzero(ptr + j, ft_strlen(dup));
+			ptr[j] = dup[j];
+			j++;
+		}
+	}
+	if (read_bytes(fd, j, dup, ptr))
+		return (1);
 	return (0);
 }
